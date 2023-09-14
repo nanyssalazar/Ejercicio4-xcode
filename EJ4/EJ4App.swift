45475e5e8df58490ed5c806c98547f11a3dfdf3c
andreassalazar <andrea.sosa01@iest.edu.mx>
2023-09-13 18:00:00 -0600

    Initial Commit

---
+++ EJ4/EJ4App.swift
@@ -0,0 +1,17 @@
+//
+//  EJ4App.swift
+//  EJ4
+//
+//  Created by -a.s  on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EJ4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
